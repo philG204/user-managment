ad039d7af9445c8165f63b9ef6b0ae6d143c58cb
philG204 <philipp.gierenz@gmail.com>
2023-04-07 16:20:17 +0200

    Added api endpoint authentication

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -3,9 +3,11 @@ import { Session } from '../entity/session.entity';
 import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Session])],
+    imports: [TypeOrmModule.forFeature([Session]), AuthModule],
     controllers: [SessionsController],
     providers: [SessionsService],
 })
