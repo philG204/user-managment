5aaf6cfda5fa2008f5f52f10a8ed83aab59d2bbe
philG204 <philipp.gierenz@gmail.com>
2023-04-10 15:23:08 +0200

    Edit AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     providers: [AuthService, AuthGuard],
     exports: [AuthService, AuthGuard],
-    imports: [AuthModule],
+    imports: [AuthModule, ConfigModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Config } from 'src/interface/config.interface';
-import * as config from '@nestjs/config';
+import { ConfigService }  from '@nestjs/config';
 import * as fs from 'fs';
 import * as crypto from 'crypto';
 
@@ -8,7 +8,7 @@ import * as crypto from 'crypto';
 export class AuthService {
     private readonly config: Config;
 
-    constructor(private readonly configService: config.ConfigService){
+    constructor(private readonly configService: ConfigService){
         this.config = JSON.parse(fs.readFileSync(this.configService.get<string>('API_KEY_DIRECTORY') + this.configService.get<string>('API_KEY_FILE'), 'utf-8')) as Config;
         console.log(this.config.apiKey);
     }
